b11467785cdd2f29ac5869fba66bb62f77141044
Axrich <116589250+Axrich@users.noreply.github.com>
2023-12-21 13:30:01 +0500

    Update LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter printer = new LeapYearPrinter();
+
+        printer.isLeapYear(2000);
+        printer.isLeapYear(2021);
+        printer.isLeapYear(2024);
     }
 }
