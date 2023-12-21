6e13ba9fabb437787eebdcea451fb637f4d42af2
Axrich <116589250+Axrich@users.noreply.github.com>
2023-12-21 13:30:24 +0500

    Update SwitchDaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,37 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int daysInMonth;
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = 30;
+                break;
+            case 2:
+                daysInMonth = 28;
+                break;
+            default:
+                System.out.println("Invalid month");
+                return;
+        }
+
+        System.out.println("Number of days in month " + month + ": " + daysInMonth);
+    }
+
+    public static void main(String[] args) {
+        SwitchDaysInMonthPrinter printer = new SwitchDaysInMonthPrinter();
+        printer.amountOfDays(7);
     }
 }
